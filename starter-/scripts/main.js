4817b1092bba8bf3967db31704f28d42175d855c
KVoronova <voronova.vs@gmail.com>
2025-07-16 22:57:04 +0300

    media and final project

--- starter-/scripts/main.js
+++ starter-/scripts/main.js
@@ -16,12 +16,20 @@ const swiper = new Swiper('.head-slider', {
     effect: 'slide', 
     speed: 1000,
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
     mousewhell: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30  
+        }
     },
     on: {
         init: function() {
