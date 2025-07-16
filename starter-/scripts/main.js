6890416fe551026f6a638c11a933541f220b72ec
KVoronova <voronova.vs@gmail.com>
2025-07-16 21:35:58 +0300

    slider and main block

--- starter-/scripts/main.js
+++ starter-/scripts/main.js
@@ -1,7 +1,8 @@
+import Swiper from '../vendor/swiper/swiper-bundle.min.mjs'
+
 import { themeSwitcher } from '../vendor/theme-switcher/theme-switcher.js'
 new themeSwitcher()
 
-// document.body.hidden = true // test
 
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.main-menu ul');
@@ -10,3 +11,38 @@ menuBtn.addEventListener('click', function() {
     this.classList.toggle('active')
     menu.classList.toggle('active')
 })
+
+const swiper = new Swiper('.head-slider', {
+    effect: 'slide', 
+    speed: 1000,
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    mousewhell: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    on: {
+        init: function() {
+            updateSlideCounter(this)
+        },
+        slideChange: function() {
+            updateSlideCounter(this)
+        }
+    }
+
+})
+
+function updateSlideCounter(swiperInstance) {
+    const realIndex = swiperInstance.realIndex + 1;
+    const totalSlides = swiperInstance.slides.length;
+    const counterElement = document.querySelector('.slider-bottom__count .value');
+
+    if(counterElement) {
+        counterElement.textContent = `${realIndex} / ${totalSlides}`
+    } else {
+        console.warn('Element slider-bottom__count not found');
+    }
+
+}
